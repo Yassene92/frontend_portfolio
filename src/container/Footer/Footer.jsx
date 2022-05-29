7ab3f1d2b427fe4bd9b6fbf3c1e38e924692d7bf
Yassine el Habbazie <yassinehabbazi@hotmaile.com>
2022-05-29 21:14:18 +0100

    add email

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -47,8 +47,8 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
-            hello@micael.com
+          <a href="mailto:yassinehabbazi@hotmail.com" className="p-text">
+            yassinehabbazi@hotmail.com
           </a>
         </div>
         <div className="app__footer-card">
